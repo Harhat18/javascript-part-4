2d8da1e5d10ddece1bb1c3674a172e55b313865a
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-09-25 21:33:37 +0300

    add

--- 3-music_player_controls/ music-player/app.js
+++ 3-music_player_controls/ music-player/app.js
@@ -9,53 +9,52 @@ const next = document.querySelector("#controls #next");
 const player = new MusicPlayer(musicList);
 
 window.addEventListener("load", () => {
-    let music = player.getMusic();
-    displayMusic(music);
+  let music = player.getMusic();
+  displayMusic(music);
 });
 
 function displayMusic(music) {
-    title.innerText = music.getName();
-    singer.innerText = music.singer;
-    image.src = "img/" + music.img;
-    audio.src = "mp3/" + music.file;
+  title.innerText = music.getName();
+  singer.innerText = music.singer;
+  image.src = "img/" + music.img;
+  audio.src = "mp3/" + music.file;
 }
 
 play.addEventListener("click", () => {
-    const isMusicPlay = container.classList.contains("playing");
-    isMusicPlay ? pauseMusic() : playMusic();
+  const isMusicPlay = container.classList.contains("playing");
+  isMusicPlay ? pauseMusic() : playMusic();
 });
 
 prev.addEventListener("click", () => {
-    prevMusic();
+  prevMusic();
 });
 
 next.addEventListener("click", () => {
-    nextMusic();
+  nextMusic();
 });
 
 function prevMusic() {
-    player.prev();
-    let music = player.getMusic();
-    displayMusic(music);
-    playMusic();
+  player.prev();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
 }
 
 function nextMusic() {
-    player.next();
-    let music = player.getMusic();
-    displayMusic(music);
-    playMusic();
+  player.next();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
 }
 
 function pauseMusic() {
-    container.classList.remove("playing");
-    play.classList = "fa-solid fa-play";
-    audio.pause();
+  container.classList.remove("playing");
+  play.classList = "fa-solid fa-play";
+  audio.pause();
 }
 
 function playMusic() {
-    container.classList.add("playing");
-    play.classList = "fa-solid fa-pause";
-    audio.play();
+  container.classList.add("playing");
+  play.classList = "fa-solid fa-pause";
+  audio.play();
 }
-

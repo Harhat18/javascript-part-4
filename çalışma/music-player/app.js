8e25b317cbe4cf60bdb430681465e7a54b9f6af8
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-09-25 09:17:12 +0300

    add

--- çalışma/music-player/app.js
+++ çalışma/music-player/app.js
@@ -1,24 +1,25 @@
+const container = document.querySelector(".container");
+const image = document.querySelector("#music-image");
+const title = document.querySelector("#music-details .title");
+const singer = document.querySelector("#music-details .singer");
+const prev = document.querySelector("#controls #prev");
+const play = document.querySelector("#controls #play");
+const next = document.querySelector("#controls #next");
+
 const player = new MusicPlayer(musicList);
 
-let music = player.getMusic();
-console.log(music.getName());
+window.addEventListener("load", () => {
+  let music;
+  player.getMusic;
+  displayMusic(music);
+});
 
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.previous();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
+function displayMusic(music) {
+  title.innerText = music.getName();
+  singer.innerText = music.singer;
+  image.src = "img/" + music.img;
+  audio.src = "mp3/" + music.file;
+}
+play.addEventListener("click", () => {
+  audio.play();
+});

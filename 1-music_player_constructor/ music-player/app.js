9d7c6661152227f0fc71f412ee75e9475a66d6d8
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-09-25 08:51:14 +0300

    add

--- 1-music_player_constructor/ music-player/app.js
+++ 1-music_player_constructor/ music-player/app.js
@@ -3,22 +3,3 @@ const player = new MusicPlayer(musicList);
 let music = player.getMusic();
 console.log(music.getName());
 
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.previous();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
-
-player.next();
-music = player.getMusic();
-console.log(music.getName());
